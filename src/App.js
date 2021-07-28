b7a65fdf01456a222df85ad5ef2817f13fd26dda
Daniel <guimaraes15@gmail.com>
2021-07-28 17:47:42 -0300

    feat: coded add and remove todo into list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [itemsList, setItemsList] = useState([]);
+
+  const handleInputTodo = (event) => {
+    let inputValue = event.target.value;
+    setTodo(inputValue);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault();
+    if (todo) {
+      setItemsList([...itemsList, todo]);
+      document.querySelector("#textToDo").value = "";
+      setTodo("");
+    }
+  };
+
+  const onItemFinished = (index) => {
+    const filteredItems = itemsList.filter(
+      (itemCurrent) => itemsList.indexOf(itemCurrent) !== index
+    );
+
+    setItemsList(filteredItems);
+  };
+
   return (
     <div className="App">
       <h2>ToDo List</h2>
       <header className="App-header">
-        <input type="text" placeholder="To-Do Title"/>
-        <button class="App-link">+</button>
+        <form>
+          <input
+            type="text"
+            placeholder="ToDo Title"
+            id="textToDo"
+            onChange={handleInputTodo}
+          />
+          <button
+            type="submit"
+            className="App-link"
+            title="Add"
+            onClick={addItem}
+          >
+            +
+          </button>
+        </form>
       </header>
       <main>
         <ul>
-          <li>Lista</li>
+          {itemsList.map((item, index) => (
+            <li key={index} onClick={() => onItemFinished(index)}>
+              {item}
+            </li>
+          ))}
         </ul>
       </main>
     </div>
